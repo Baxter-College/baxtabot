af2f31493e02bc79409bfd1585cba4bb32f3a5f4
Tom Hill <tomhill98@me.com>
2018-08-10 20:57:12 +1000

    I think I might cry

--- app.py
+++ app.py
@@ -109,8 +109,8 @@ def callSendAPI(sender_psid, response):
 	r = requests.post(
 		"https://graph.facebook.com/v2.6/me/messages",
 		params = { "access_token": PAGE_ACCESS_TOKEN },
-		data = {
-			"messaging_type": "RESPONSE", # alternatively MESSAGE_TAG
+		json = {
+			#"messaging_type": "RESPONSE", # alternatively MESSAGE_TAG
 			"recipient": {
 				"id": sender_psid
 			},
