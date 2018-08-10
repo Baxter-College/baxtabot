1de28f3ac773305e0ee7a6fbc731f0369e181acd
Tom Hill <tomhill98@me.com>
2018-08-11 09:42:52 +1000

    I'm an idiot.

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ import models
 
 app = Flask(__name__)
 
-#DEBUG = int(os.environ.get('DEBUG'))
-#PORT = int(os.environ.get('PORT'))
-DEBUG = 1
-PORT = 8000
+DEBUG = int(os.environ.get('DEBUG'))
+PORT = int(os.environ.get('PORT'))
+# DEBUG = 1
+# PORT = 8000
 
 PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 VERIFY_TOKEN = "GoodLordyThomasJHillLooksFineTonight"
