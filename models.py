83705345440cfda12eab944e0aad1b53a7e7f275
Tom Hill <tomhill98@me.com>
2018-08-10 23:08:43 +1000

    Ill get it one day

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 import datetime
 import psycopg2
 import os
+import urlparse
 
 from peewee import *
 
 if 'HEROKU' in os.environ:
-	db = PostgresqlDatabase(os.environ['DATABASE_URL'])
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
 else:
 	db = PostgresqlDatabase('baxtabot', user='tomhill', password='angular')
 
