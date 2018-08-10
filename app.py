57e8fc065594e7db205e81b15462bb66d6228ba9
Tom Hill <tomhill98@me.com>
2018-08-10 20:46:38 +1000

    is logging working?

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import os
 import json
 import requests
 
+
+import logging
+import http.client as http_client
+
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 app = Flask(__name__)
 
 DEBUG = int(os.environ.get('DEBUG'))
