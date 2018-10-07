d87268470cc00dc31a5ccc878b541510db94cc3d
Tom Hill <tomhill98@me.com>
2018-10-07 23:53:09 +1100

    Fixed error

--- message.py
+++ message.py
@@ -127,7 +127,7 @@ def handleMessage(sender_psid, received_message):
 	elif ("room is" in received_message):
 		#name = functions.extractName(received_message)
 		#response = { "text": functions.getRoomNumber(name) }
-		response = {"text": "I can't assist a murder!!! My room number function has been disabled for assassan's week. Watch your back."}
+		response = {"text": "I can't assist a murder!!! My room number function has been disabled for assassin's week. Watch your back."}
 
 	else:
 		reply = bot.reply(str(sender_psid), received_message)
