bf2bd499f29620478f978b2889d3a960d7b42ee1
Tom Hill <tomhill98@me.com>
2019-07-04 18:22:46 +1000

    image?

--- bot/message.py
+++ bot/message.py
@@ -82,10 +82,10 @@ def handleMessage(sender_psid, received_message):
             response.add_reply(Reply("Whats dino like?"))
             response.add_reply(Reply("Dinovote"))
 
-            if theMeal.images:
-                image = random.choice([image for image in meal.images])
-                Response(sender_psid, image=image.url).send()
-                Response(sender_psid, f"Photo by: {image.sender.full_name}").send()
+            # if theMeal.images:
+            #     image = random.choice([image for image in meal.images])
+            #     Response(sender_psid, image=image.url).send()
+            #     Response(sender_psid, f"Photo by: {image.sender.full_name}").send()
 
     elif (
         "dinopoll" in received_message
