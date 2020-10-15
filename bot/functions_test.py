9390e35de870e00a0943a6df31b62769c14ecc5e
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2020-10-15 11:45:30 +1100

    added first test file

---
+++ bot/functions_test.py
@@ -0,0 +1,19 @@
+'''
+Test file for functions in functions.py (that are testable...)
+'''
+
+from functions import findMeal, findTime, extractName
+
+def test_findMeal():
+    assert findMeal('What is for dinner') == 'dinner'
+    assert findMeal('I hope breakfast is good today') == 'breakfast'
+    assert findMeal('tell me what is lunch') == 'lunch'
+
+def test_findTime():
+    pass
+
+def test_extractName():
+    pass
+
+def test_extractRessieFromCSV():
+    pass
