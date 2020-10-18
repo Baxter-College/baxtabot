cde8714efd9b2999613de7b528a634c9162f46c4
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2020-10-18 18:11:31 +1100

    query now working somewhat

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def privacy():
 
 @app.route('/latemeals')
 def latemeals():
-    meals = models.LateMeal.select(models.LateMeal, models.Ressie, models.Meal).join(models.Ressie).switch(models.LateMeal).join(models.Meal)
+    meals = models.LateMeal.select(models.LateMeal.notes, models.Ressie.first_name, models.Meal.description).join(models.Ressie).switch(models.LateMeal).join(models.Meal)
     for meal in meals:
         print(meal)
     return render_template('latemeals.html', meals=meals)
