4e5feaec54e430b18e40f1a6d187539d4e92fb7f
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2020-10-18 19:29:31 +1100

    using dicts

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def privacy():
 
 @app.route('/latemeals')
 def latemeals():
-    meals = models.LateMeal.select(models.LateMeal.notes, models.Ressie.first_name, models.Meal.description).join(models.Ressie).switch(models.LateMeal).join(models.Meal)
+    meals = models.LateMeal.select(models.LateMeal.notes, models.Ressie.first_name, models.Meal.description).join(models.Ressie).switch(models.LateMeal).join(models.Meal).dicts()
     for meal in meals:
         print(meal)
     return render_template('latemeals.html', meals=meals)
