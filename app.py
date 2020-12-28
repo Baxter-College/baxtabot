081dd02c52d3b0ee3994643f77cc74d637aaf503
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2020-12-28 20:29:04 +0800

    users now has access to id

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def users():
     elif not functions.validateTokenPermissions(token):
         return render_template('homepage.html', permission_denied=True)
     else:
-        users = models.Client.select(models.Client.name, models.Client.email, models.Client.position, models.ClientPermissions.dinoread,
+        users = models.Client.select(models.Client.id, models.Client.name, models.Client.email, models.Client.position, models.ClientPermissions.dinoread,
                                     models.ClientPermissions.dinowrite, models.ClientPermissions.calendar, models.ClientPermissions.latemeals,
                                     models.ClientPermissions.ressies, models.ClientPermissions.sport, models.ClientPermissions.users).join(models.ClientPermissions).dicts()
         print(users[0])
