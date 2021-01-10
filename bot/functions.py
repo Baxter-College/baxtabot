c75e1322b5f843729543dfca3d690364ae3ed54f
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2021-01-10 17:37:43 +0800

    get room number client problem rmoved

--- bot/functions.py
+++ bot/functions.py
@@ -424,9 +424,12 @@ def getRoomNumber(name):
 
     try:
         gotName, confidence, ressie = models.Ressie.fuzzySearch(name)
-        client = models.Client.select().join(models.Ressie).where(models.Ressie.id == ressie.id).get()
-        if not client.roomshown:
-            return '{} is in baxter and has turned room sharing off'.format(gotName)
+        client = models.Client.select().join(models.Ressie).where(models.Ressie.id == ressie.id)
+        if client:
+            client = client.get()
+            
+            if not client.roomshown:
+                return '{} is in baxter and has turned room sharing off'.format(gotName)
 
         if confidence < 85:
             return "{} is in room {} (I'm {} percent sure I know who you're talking about)".format(
