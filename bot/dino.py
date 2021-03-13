735fda700be8c82d8648539cc209e66e2198e099
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2021-03-13 15:47:17 +1100

    setup of test files and started work on docstring comments

--- bot/dino.py
+++ bot/dino.py
@@ -3,14 +3,41 @@ from bot.models import Meal
 import bot.functions as functions
 
 def meals_all():
+    '''
+    Return a list of all meals
+    '''
     return Meal.select().order_by(Meal.date.desc())
 
 def meals_add(date, description, type):
+    '''
+    Add a new meal
+
+    Parameters:
+    - date
+    - description
+    - type
+
+    Exceptions:
+    - InputError: date is not valid
+    - InputError: description is empty
+    - InputError: type not in ['breakfast', 'lunch', 'dinner']
+
+    Return value: None
+    '''
     Meal.create(
         date=date, description=description, type=type
     )
 
 def meals_delete(meal_id):
+    '''
+    Deletes an existing meal
+
+    Paramters:
+    - meal_id
+
+    Exceptions:
+    - InputError: meal_id is not valid
+    '''
     meal = Meal.select().where(Meal.id == meal_id).get()
     meal.delete_instance()
 
---
+++ bot/latemeals_test.py
@@ -0,0 +1,18 @@
+import bot.latemeals as latemeals
+from bot.testutils import clear
+from bot.error import InputError
+
+def test_latemeal_create():
+    pass
+
+def test_latemeal_setcompleted_success():
+    pass
+
+def test_latemeal_setcompleted_invalid():
+    pass
+
+def test_latemeals_oustanding_resident_success():
+    pass
+
+def test_latemeals_oustanding_resident_invalidid():
+    pass
