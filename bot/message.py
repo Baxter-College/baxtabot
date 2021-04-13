d25f7f206511f3de101490628958cb18f4185b40
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2021-04-13 21:30:52 +0800

    removed voting altogether

--- bot/message.py
+++ bot/message.py
@@ -282,10 +282,6 @@ def handleMessage(sender_psid, received_message):
         # response.asset = "270145943837548"
         url = 'https://indomie.com.au/wp-content/uploads/2020/03/migorengjumbo-new.png'
         Response(sender_psid, image=url).send()
-    '''elif 'voted' in received_message:
-        data = functions.humanisePSID(sender_psid)
-        Response(3973058702707960, data['first_name'] + ' ' + data['last_name'] + ' has voted!').send()
-        response.text = 'Thanks for your support! Have a great dino meal''''
     elif (
         "dino is shit" in received_message
         or "dino is bad" in received_message
